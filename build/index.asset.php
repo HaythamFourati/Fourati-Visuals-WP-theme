9d86e8ec5249239239ae80ee9826c3549c8655b7
HaythamFourati <shadowfourati@gmail.com>
2025-02-12 08:58:09 +0100

    Test

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'd0d569d5c632f0d62333');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '7fe7dc3df97c6f714de9');
