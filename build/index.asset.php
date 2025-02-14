a4576a3ae64e97816edca9cbd080b52541c3e0f5
HaythamFourati <shadowfourati@gmail.com>
2025-02-14 07:22:20 +0100

    BG Edit

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '933ddb94276b865d8b27');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '6a42c03847e993fb2c40');
